f10f0fffefc5420aea9616971173944018bfe21a
chriszf <chriszf@gmail.com>
2012-10-24 00:08:19 -0700

    Completed basic workflow

--- model.py
+++ model.py
@@ -9,8 +9,14 @@ def connect_db():
 def new_user(db, email, password, name):          
     c = db.cursor()                                     
     query = """INSERT INTO Users VALUES (NULL, ?, ?, ?)"""                                                           
-    c.execute(query, (email, password, name))           
-    db.commit()
+    res = c.execute(query, (email, password, name))           
+    if res:
+        db.commit()
+        return res.lastrowid
+
+def make_user(row):
+    fields = ["id", "email", "password", "username"]
+    return dict(zip(fields, row))
 
 def authenticate(db, email, password):
     c = db.cursor()
@@ -19,21 +25,40 @@ def authenticate(db, email, password):
     result = c.fetchone()
     if result:
         fields = ["id", "email", "password", "username"]
-        return dict(zip(fields, result))
+        return make_user(result)
 
     return None
 
 def get_user(db, user_id):
     """Gets a user dictionary out of the database given an id"""
-    pass
+    c = db.cursor()
+    query = """SELECT * from Users WHERE id = ?"""
+    c.execute(query, (user_id,))
+    user_row = c.fetchone()
+    if user_row:
+        return make_user(user_row)
 
-def new_task(db, title, user_id):
+    return None
+
+def new_task(db, title, user_id = None):
     """Given a title and a user_id, create a new task belonging to that user. Return the id of the created task"""
-    pass
+    c = db.cursor()
+    query = """INSERT into Tasks values (null, ?, DATETIME('now'), null, ?)"""
+    res = c.execute(query, (title, user_id))
+    if res:
+        db.commit()
+        return res.lastrowid
 
 def complete_task(db, task_id):
     """Mark the task with the given task_id as being complete."""
-    pass
+    c = db.cursor()
+    query = """UPDATE Tasks SET completed_at=DATETIME('now') WHERE id=?"""
+    res = c.execute(query, (task_id,))
+    if res:
+        db.commit()
+        return res.lastrowid
+    else:
+        return None
 
 def get_tasks(db, user_id=None):
     """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
@@ -47,10 +72,22 @@ def get_tasks(db, user_id=None):
     tasks = []
     rows = c.fetchall()
     for row in rows:
-        task = dict(zip(["id", "title", "created_at", "completed_at", "user_id"], row))
+        task = make_task(row)
         tasks.append(task)
 
     return tasks
 
 def get_task(db, task_id):
     """Gets a single task, given its id. Returns a dictionary of the task data."""
+    c = db.cursor()
+    query = """SELECT * from Tasks WHERE id = ?"""
+    c.execute(query, (task_id,))
+    task_row = c.fetchone()
+    if task_row:
+        return make_task(task_row)
+
+    return None
+
+def make_task(row):
+    columns = ["id", "title", "created_at", "completed_at", "user_id"]
+    return dict(zip(columns, row))
--- tipsy.py
+++ tipsy.py
@@ -1,7 +1,7 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import model
 
 app = Flask(__name__)
@@ -10,11 +10,30 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", user_name="chriszf")
 
+@app.route("/save_task", methods=["POST"])
+def save_task():
+    db = model.connect_db()
+    title = request.form['title']
+    model.new_task(db, title)
+    return redirect("/tasks")
+
 @app.route("/tasks")
 def list_tasks():
     db = model.connect_db()
     tasks_from_db = model.get_tasks(db, None)
     return render_template("list_tasks.html", tasks=tasks_from_db)
+
+@app.route("/task/<int:id>", methods=["GET"])
+def view_task(id):
+    db = model.connect_db()
+    task_from_db = model.get_task(db, id)
+    return render_template("view_task.html", task=task_from_db)
+
+@app.route("/task/<int:id>", methods=["POST"])
+def complete_task(id):
+    db = model.connect_db()
+    model.complete_task(db, id)
+    return redirect("/tasks")
 
 if __name__ == "__main__":
     app.run(debug=True)
